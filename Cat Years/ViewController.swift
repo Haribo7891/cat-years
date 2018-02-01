c881b19c05bad27e88a61e8da14d106d0597b234
Haribo7891 <harry.crank@me.com>
2018-02-01 13:08:55 +0000

    Cat Years App updated from Swift 2.0

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var catAgeTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
 
     override func viewDidLoad() {
+        
+        catAgeTextField.delegate = self
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        catAgeTextField.resignFirstResponder()
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let allowedCharacters = CharacterSet.decimalDigits
+        let characterSet = CharacterSet(charactersIn: string)
+        return allowedCharacters.isSuperset(of: characterSet)
     }
 
-
+    @IBAction func findAge(_ sender: UIButton) {
+        
+        catAgeTextField.resignFirstResponder()
+        
+        var catAge = Int(catAgeTextField.text!)!
+        
+        catAge = catAge * 7
+        
+        resultLabel.text = "Your cat is \(catAge) in human years"
+    }
+    
 }
 
